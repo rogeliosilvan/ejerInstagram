dd28ec2c60b6a59303cac9eaa8c288f0129b5759
rogelio silvan <rogoffspring@hotmail.com>
2018-11-06 09:49:52 +0100

    commit inicial

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const webpack = require("webpack");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+};
